2466992109c7eb32d1bd144bce2ade6e90d51b18
Julien Vernet <julien@vernet.me>
2014-07-17 15:20:05 +0700

    Make it work with SSL

--- js/app-config.js
+++ js/app-config.js
@@ -5,9 +5,9 @@ $(function () {
 
 	var beUsername = 'your_behance_username',
 		beApiKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-		beUserAPI = 'http://www.behance.net/v2/users/' + beUsername + '?callback=?&api_key=' + beApiKey,
+		beUserAPI = '//www.behance.net/v2/users/' + beUsername + '?callback=?&api_key=' + beApiKey,
 		bePerPage = 12,
-		beProjectAPI = 'http://www.behance.net/v2/users/' + beUsername + '/projects?callback=?&api_key=' + beApiKey + '&per_page=' + bePerPage;
+		beProjectAPI = '//www.behance.net/v2/users/' + beUsername + '/projects?callback=?&api_key=' + beApiKey + '&per_page=' + bePerPage;
 
 	////////////////////////
 	// Behance User data //
